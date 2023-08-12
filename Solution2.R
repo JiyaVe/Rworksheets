5f10de8a27080f51e523a2b9c877c53d20214999
JiyaVe <121509919+JiyaVe@users.noreply.github.com>
2023-08-12 11:56:50 +0530

    Add files via upload
    Worksheet4

--- Solution2.R
+++ Solution2.R
@@ -5,6 +5,8 @@
 #The point with the smallest distance will be closest to the point (0,1,0)purest green.
 
 #To find purest Green
+library(imager)
+
 dist <- matrix(0, nrow = dim(dog)[1], ncol = dim(dog)[2])
 
 for (i in 1:dim(dog)[1])
@@ -22,3 +24,27 @@ points(index , col = "Red" ,pch = 16)
 
 #2
 #To find purest red and blue change the vector to (1,0,0) and (0,0,1)
+
+#3
+
+im1 <- load.image("col1.png")
+im2 <- load.image("col2.png")
+im3 <- load.image("col3.png")
+
+
+for (i in c(im2,im3))
+{
+ if (norm(i[5,5,1,] - c(1,0,0), "2") <0.6 & (norm(i[5,5,1,] - c(1,0,0)>=0)))
+ {
+   print(i," is Red.")
+ }
+  else if (norm(i[5,5,1,] - c(0,1,0), "2") <0.6 & (norm(i[5,5,1,] - c(0,1,0)>=0)))
+    {
+      print(i," is Green.")
+    }
+  else
+  {
+    print(i, "is Blue.")
+  }
+  
+}
