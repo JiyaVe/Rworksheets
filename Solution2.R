14d10351ad64fdb6c6a13478e14e4633d9202e91
JiyaVe <121509919+JiyaVe@users.noreply.github.com>
2023-08-17 13:05:29 +0530

    WORKSHEET4

--- Solution2.R
+++ Solution2.R
@@ -1,47 +1,108 @@
-#Q2
-#Cricketer Info
+#Questions
 
-cricket <- read.csv("battingbowling.csv")
+#1
+#Logic: Run loop throughout the pixels and calculate the euclidian distance of the pixels. 
+#The point with the smallest distance will be closest to the point (0,1,0)purest green.
 
-cricket
+#To find purest Green
+library(imager)
+dog <- load.image("dog.jpeg")
 
-player <- subset(cricket, Bowling <= 40 & Batting >= 25)
+dist <- matrix(0, nrow = dim(dog)[1], ncol = dim(dog)[2])
+
+for (i in 1:dim(dog)[1])
+{
+  for (j in 1:dim(dog)[2])
+  {
+  dist[i,j] <- norm(dog[i,j,1,] - c(0,1,0), "2")
+    
+  }
+}
+
+index <- which(dist == min(dist), arr.ind = TRUE)
+plot(dog)
+points(index , col = "Red" ,pch = 16)
+
+#2
+#To find purest red and blue change the vector to (1,0,0) and (0,0,1)
+
+#3
+col1 <- load.image("col1.png")
+dim(col1)
+
+col2 <- load.image("col2.png")
+dim(col2)
+
+col3 <- load.image("col3.png")
+dim(col3)
+
+colour<-c("red","green","blue")
+
+index1 <- which(col1[1,1,1,1:3]>0.5)
+colour[index1]
+
+index2 <- which(col2[1,1,1,1:3]>0.5)
+colour[index2]
+
+index3 <- which(col3[1,1,1,1:3]>0.5)
+colour[index3]
+
+#3
+col1 <- load.image("col1.png")
+dim(col1)
+col2 <- load.image("col2.png")
+dim(col2)
+col3 <- load.image("col3.png")
+dim(col3)
+
+colour<-c("red","green","blue")
+index1 <- which.max(col1[1,1,1,1:3])
+index1
+colour[index1]
+
+index2 <- which.max(col2[1,1,1,1:3])
+colour[index2]
 
 
-n1 <- subset(player,Team == "India") 
-n2 <- subset(player,Team == "Australia") 
-n3 <- subset(player,Team == "England") 
-n4 <- subset(player,Team == "New Zealand") 
-n5 <- subset(player,Team == "Pakistan")
-n6 <- subset(player,Team == "South Africa") 
+index3 <- which.max(col3[1,1,1,1:3])
+index3
+colour[index3]
+
+#3 (More Accurate)
+col1 <- load.image("col1.png")
+col1 <- as.cimg(col1[ , , , 1:3])
+col2 <- load.image("col2.png")
+col3 <- load.image("col3.png")
+
+diff.col <- function(img, col)
+{
+  col.mat <- as.array(img[, , 1, ])
+  dims <- dim(col.mat)
   
-n <- max(dim(n1)[1],dim(n2)[1],dim(n3)[1],dim(n4)[1],dim(n5)[1],dim(n6)[1])
-m <- min(dim(n1)[1],dim(n2)[1],dim(n3)[1],dim(n4)[1],dim(n5)[1],dim(n6)[1])
+  dist <- matrix(0, nrow = dims[1], ncol = dims[2])
+  for(i in 1:dims[1])
+  {
+    for(j in 1:dims[2])
+    {
+      dist[i,j] <- norm(col.mat[i,j, ] - col, "2")
+    }
+  }
+  return(dist)
+}
+which.color <- function(img)
+{
+  dist.cols <- numeric(length = 3)
+  colors.matrix <- diag(3)  
+  for(k in 1:3)
+  {
+    dist.cols[k] <- mean(diff.col(img, col = colors.matrix[ ,k]))
+  }
+  ind <- which.min(dist.cols)
+  guess <- c("red", "green", "blue")[ind]
+  
+  return(guess)
+}
 
-
-if (dim(n1)[1] == n) {print(n1$Team[1])}
-if (dim(n2)[1] == n) {print(n2$Team[1])}
-if (dim(n3)[1] == n) {print(n3$Team[1])}
-if (dim(n4)[1] == n) {print(n4$Team[1])}
-if (dim(n5)[1] == n) {print(n5$Team[1])}
-if (dim(n6)[1] == n) {print(n6$Team[1])}
-
-if (dim(n1)[1] == m) {print(n1$Team[1])}
-if (dim(n2)[1] == m) {print(n2$Team[1])}
-if (dim(n3)[1] == m) {print(n3$Team[1])}
-if (dim(n4)[1] == m) {print(n4$Team[1])}
-if (dim(n5)[1] == m) {print(n5$Team[1])}
-if (dim(n6)[1] == m) {print(n6$Team[1])}
-
-
-cricket <- read.csv("battingbowling.csv")
-
-
-## Problem 2(a)
-allround_index <- (cricket$Batting > 25 & cricket$Bowling < 40)
-allround <- cricket[allround_index, ]
-allround
-
-tab <- table(allround$Team)
-which.min(tab)
-which.max(tab)
+which.color(col1)  
+which.color(col2)  
+which.color(col3)
--- Solution3.R
+++ Solution3.R
@@ -1,14 +1,69 @@
-#Q3
+#4
 
-plot(c(1:10),c(1:10),type="l")
+library(imager)
+name1 <- readline("Enter image file name1: ")
+img1 <- load.image(name1)
 
-x <- 0:10
+dist <- matrix(0,nrow = dim(img1)[1], ncol = dim(img1)[2])
 
-plot(y <- x,type = "l")
+for (i in 1:dim(img1)[1])
+{
+  for (j in 1:dim(img1)[2])
+  {
+    dist[i,j] <- norm(img1[i,j,1,] - c(1,1,1), "2")
+  }
+}
 
-#Q4
+index1 <- which(dist == min(dist), arr.ind = TRUE)
 
-n <- 1:1000
-f <- (n+(1/n))^n
-plot(f,type="l")
-abline(v=exp(1),col="red")
+
+
+name2 <- readline("Enter image file name2: ")
+img2 <- load.image(name2)
+
+dist2 <- matrix(0,nrow = dim(img2)[1], ncol = dim(img2)[2])
+
+for (i in 1:dim(img2)[1])
+{
+  for (j in 1:dim(img2)[2])
+  {
+    dist2[i,j] <- norm(img2[i,j,1,] - c(1,1,1), "2")
+  }
+}
+
+index2 <- which(dist2 == min(dist2), arr.ind = TRUE)
+
+if ( dim(index1)[1]> dim(index2)[1]) { print("Image 1 has a lot of snow.")}
+if( dim(index2)[1]> dim(index1)[1]) { print("Image 2 has a lot of snow.")}
+
+#5 #6 #7
+n <- readline("Enter the image file name: ")
+i <- load.image(n)
+new180 <- imrotate(i,180)
+plot(new180)
+
+new90 <- imrotate(i,90)#cloackwise
+plot(new90)
+
+new1 <- imrotate(i,270)#anticlockwise
+plot(new1)  
+  
+
+#8 #9
+
+dog <- load.image("dog.jpeg")
+col_mat <- as.array(dog[,,1,])
+dim(col_mat)
+
+cropped <- col_mat[1:600,1:600,]
+crop_dog <- as.cimg(cropped)
+plot(crop_dog)
+
+resized_img <- resize(crop_dog,  size_x = 300L, size_y = 300L)
+plot(resized_img)
+save.image(resized_img,"resized_img.jpeg")
+
+resized_img2 <- resize(crop_dog,  size_x = 60L, size_y = 60L)
+plot(resized_img2)
+
+
